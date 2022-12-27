21db841b36958e0be0b79366d185ed6120929da1
Dennis Akagha <dennis.akagha@afripointgroup.com>
2022-12-27 21:55:46 +0100

    Add CRUD using serializers

--- posts/models.py
+++ posts/models.py
@@ -3,5 +3,9 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.models.TextField()
+    content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
