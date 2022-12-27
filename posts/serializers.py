2429a9c765a90fcd18907f1cd54e72a3147bb368
Dennis Akagha <dennis.akagha@afripointgroup.com>
2022-12-27 22:05:09 +0100

    add validation

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 
 class PostModelSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=50)
     class Meta:
         model =  Post
         fields = ['title', 'content', 'created']
